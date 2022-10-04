03d29e381677e1886886aeaff4b286622ed7d195
Rubanov Andrey <nirwash@bk.ru>
2022-10-04 10:00:15 +0300

    add click listener on rate button

--- app/src/main/java/com/nirwashh/android/mycryptoapp/activities/AboutActivity.kt
+++ app/src/main/java/com/nirwashh/android/mycryptoapp/activities/AboutActivity.kt
@@ -1,5 +1,7 @@
 package com.nirwashh.android.mycryptoapp.activities
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nirwashh.android.mycryptoapp.R
@@ -14,5 +16,9 @@ class AboutActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.buttonRateApp.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${applicationContext.packageName}")))
+        }
+
     }
 }
